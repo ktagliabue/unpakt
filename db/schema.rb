6784f97aecd9223f7ed12ad629febc70e5868810
Kandis <kandistag@gmail.com>
2016-09-28 10:45:11 -0700

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160928054258) do
+ActiveRecord::Schema.define(version: 20160928054727) do
 
   create_table "closets", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -34,6 +34,11 @@ ActiveRecord::Schema.define(version: 20160928054258) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_models_on_email", unique: true
     t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
